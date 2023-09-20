2a7517a704675bc9673b40a4272774952ce5029b
Steve Montgomery <steve@chums.com>
2023-09-20 09:48:37 -0600

    feat: add product where used lookup

--- lib/product/v2/utils.ts
+++ lib/product/v2/utils.ts
@@ -8,10 +8,12 @@ import {
     CategoryChildLink,
     CategoryChildProduct,
     CategoryChildSection,
-    CustomerPriceRecord,
     Product,
-    ProductCategoryChild, ProductSellAsColors, ProductSellAsMix,
-    ProductSellAsSelf, ProductSellAsVariants,
+    ProductCategoryChild,
+    ProductSellAsColors,
+    ProductSellAsMix,
+    ProductSellAsSelf,
+    ProductSellAsVariants,
     SellAsColorsProduct,
     SellAsMixProduct,
     SellAsSelfProduct,
@@ -34,9 +36,9 @@ interface CheckExistingKeywordProps {
     pagetype: string,
 }
 
-export const SELL_AS_SELF:ProductSellAsSelf = 1;
-export const SELL_AS_VARIANTS:ProductSellAsVariants = 0;
-export const SELL_AS_MIX:ProductSellAsMix = 3;
+export const SELL_AS_SELF: ProductSellAsSelf = 1;
+export const SELL_AS_VARIANTS: ProductSellAsVariants = 0;
+export const SELL_AS_MIX: ProductSellAsMix = 3;
 export const SELL_AS_COLORS: ProductSellAsColors = 4;
 
 export const checkExistingKeyword = async ({id = 0, keyword, pagetype}: CheckExistingKeywordProps): Promise<void> => {
@@ -111,21 +113,21 @@ export function parseImageFilename(productImage: string, colorCode: string | num
 // };
 
 
-export const isCartItem = (item:CartItem|null): item is CartItem => {
+export const isCartItem = (item: CartItem | null): item is CartItem => {
     if (!item) {
         return false;
     }
     return (item as CartItem).itemCode !== undefined;
 }
 
-export const isCartProduct = (item:CartProduct|CartItem|null): item is CartProduct => {
+export const isCartProduct = (item: CartProduct | CartItem | null): item is CartProduct => {
     if (!item) {
         return false;
     }
     return isCartItem(item) && (item as CartProduct).productId !== undefined;
 }
 
-export const isProduct = (product:Product|null): product is Product => {
+export const isProduct = (product: Product | null): product is Product => {
     if (!product) {
         return false;
     }
@@ -148,19 +150,19 @@ export function isCategoryChildLink(child: ProductCategoryChild): child is Categ
     return (child as CategoryChildLink).itemType === 'link';
 }
 
-export function isSellAsSelf(product: Product|null): product is SellAsSelfProduct {
+export function isSellAsSelf(product: Product | null): product is SellAsSelfProduct {
     return !!product && (product as SellAsSelfProduct).sellAs === SELL_AS_SELF;
 }
 
-export function isSellAsVariants(product: Product|null): product is SellAsVariantsProduct {
+export function isSellAsVariants(product: Product | null): product is SellAsVariantsProduct {
     return !!product && (product as SellAsVariantsProduct).sellAs === SELL_AS_VARIANTS;
 }
 
-export function isSellAsMix(product: Product|null): product is SellAsMixProduct {
+export function isSellAsMix(product: Product | null): product is SellAsMixProduct {
     return !!product && (product as SellAsMixProduct).sellAs === SELL_AS_MIX;
 }
 
-export function isSellAsColors(product: Product|null): product is SellAsColorsProduct {
+export function isSellAsColors(product: Product | null): product is SellAsColorsProduct {
     return !!product && (product as SellAsColorsProduct).sellAs === SELL_AS_COLORS;
 }
 
