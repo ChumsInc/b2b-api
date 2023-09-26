4981a6471241a8e745430aa97efda6e41237ac7d
Steve Montgomery <steve@chums.com>
2023-09-26 13:22:18 -0600

    feat: add product where used lookup

--- lib/product/where-used.ts
+++ lib/product/where-used.ts
@@ -13,6 +13,7 @@ export interface WhereUsedProps {
 export interface WhereUsed {
     id: number;
     keyword: string;
+    active: boolean|number;
 }
 
 export interface WhereUsedResponse {
@@ -32,35 +33,39 @@ export async function loadWhereUsed(productId:number|string|null, itemCode: stri
 
         const sqlProductItemCode = `SELECT p.products_id as id, p.products_keyword as keyword, p.products_status as active
                                     FROM b2b_oscommerce.products p
-                                    WHERE p.products_model = :itemCode
+                                    WHERE p.products_model like :itemCode
                                     UNION
                                     SELECT p2.products_id, p2.products_keyword, p2.products_status
                                     FROM b2b_oscommerce.products p
                                              INNER JOIN b2b_oscommerce.products_variants pv ON pv.productID = p.products_id
                                              INNER JOIN b2b_oscommerce.products p2 ON p2.products_id = pv.variantProductID
-                                    WHERE p2.products_model = :itemCode
+                                    WHERE p2.products_model like :itemCode
                                     UNION
                                     SELECT p.products_id, p.products_keyword, p.products_status
                                     FROM b2b_oscommerce.products p
                                              INNER JOIN b2b_oscommerce.products_items pi ON pi.productsID = p.products_id
-                                    WHERE pi.itemCode = :itemCode
+                                    WHERE pi.itemCode like :itemCode
                                     UNION
                                     SELECT p.products_id AS id, p.products_keyword AS keyword, p.products_status
                                     FROM b2b_oscommerce.products_mixes_detail pmd
                                              INNER JOIN b2b_oscommerce.products_mixes pm ON pm.mixID = pmd.mixID
                                              INNER JOIN b2b_oscommerce.products p ON p.products_id = pm.productsID
-                                    WHERE pmd.itemCode = :itemCode`;
-        const sqlProductInCategory = `SELECT cp.categorypage_id, cp.page_keyword
+                                    WHERE pmd.itemCode like :itemCode`;
+        const sqlProductInCategory = `SELECT DISTINCT cp.categorypage_id AS id, cp.page_keyword AS keyword
                                       FROM b2b_oscommerce.category_pages cp
                                                INNER JOIN b2b_oscommerce.category_pages_items cpi
                                                           ON cpi.categorypage_id = cp.categorypage_id
                                       WHERE cpi.products_id IN (:productIds)`;
         const [vRows] = await mysql2Pool.query<WhereUsedRow[]>(sqlProductsAsVariant, {productId, itemCode});
         const [iRows] = await mysql2Pool.query<WhereUsedRow[]>(sqlProductItemCode, {productId, itemCode});
-        whereUsed.products = [...vRows, ...iRows];
-
-        const [cpRows] = await mysql2Pool.query<WhereUsedRow[]>(sqlProductInCategory, {productIds: whereUsed.products.map(p => p.id)})
-        whereUsed.categoryPages = [...cpRows];
+        whereUsed.products = [...vRows, ...iRows].map(row => ({...row, active: !!row.active}));
+        const productIds = whereUsed.products.map(p => p.id);
+        if (productIds.length > 0) {
+            const [cpRows] = await mysql2Pool.query<WhereUsedRow[]>(sqlProductInCategory, {productIds: whereUsed.products.map(p => p.id)})
+            whereUsed.categoryPages = cpRows.map(row => ({...row, active: !!row.active}));
+        } else {
+            whereUsed.categoryPages = [];
+        }
         return whereUsed;
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -80,10 +85,9 @@ export const getWhereUsed = async (req:Request, res:Response) => {
         res.json({whereUsed});
     } catch(err:unknown) {
         if (err instanceof Error) {
-            console.debug("getWhereUsed()", err.message);
-            return Promise.reject(err);
+            debug("getWhereUsed()", err.message);
+            return res.json({error: err.message, name: err.name});
         }
-        console.debug("getWhereUsed()", err);
-        return Promise.reject(new Error('Error in getWhereUsed()'));
+        res.json({error: 'unknown error in getWhereUsed'});
     }
 }
