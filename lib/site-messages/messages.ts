99120ccb368940676fe1d8f6da95b7e87248ab15
Steve Montgomery <steve@chums.com>
2025-12-01 16:02:34 -0700

    chore: refactor site-messages module, update type imports to "chums-types/b2b", clean up interfaces, improve function definitions, and enhance error handling formatting

--- lib/site-messages/messages.ts
+++ lib/site-messages/messages.ts
@@ -1,17 +1,17 @@
 import Debug from "debug";
 import {mysql2Pool} from "chums-local-modules";
-import {Message} from "b2b-types";
+import {Message} from "chums-types/b2b";
 import {ResultSetHeader, RowDataPacket} from "mysql2";
 
 const debug = Debug('chums:lib:site-messages:messages');
 
-interface MessageRow extends RowDataPacket, Omit<Message, 'active'|'allow_nextday'|'allow_twoday'> {
+interface MessageRow extends RowDataPacket, Omit<Message, 'active' | 'allow_nextday' | 'allow_twoday'> {
     active: number;
     allow_nextday: number,
     allow_twoday: number;
 }
 
-export async function loadMessages(id:number|string|null = null):Promise<Message[]> {
+export async function loadMessages(id: number | string | null = null): Promise<Message[]> {
     try {
         const query = `SELECT id,
                               type,
@@ -33,7 +33,7 @@ export async function loadMessages(id:number|string|null = null):Promise<Message
             allow_nextday: !!row.allow_nextday,
             allow_twoday: !!row.allow_twoday
         }));
-    } catch(err:unknown) {
+    } catch (err: unknown) {
         if (err instanceof Error) {
             debug("loadMessages()", err.message);
             return Promise.reject(err);
@@ -43,7 +43,7 @@ export async function loadMessages(id:number|string|null = null):Promise<Message
     }
 }
 
-export async function loadCurrentMessages():Promise<Message[]> {
+export async function loadCurrentMessages(): Promise<Message[]> {
     try {
         const now = new Date().valueOf();
         const messages = await loadMessages();
@@ -52,7 +52,7 @@ export async function loadCurrentMessages():Promise<Message[]> {
                 && (m.start === null || new Date(m.start).valueOf() <= now)
                 && (m.end === null || new Date(m.end).valueOf() >= now)
         });
-    } catch(err:unknown) {
+    } catch (err: unknown) {
         if (err instanceof Error) {
             debug("loadCurrentMessages()", err.message);
             return Promise.reject(err);
@@ -62,15 +62,22 @@ export async function loadCurrentMessages():Promise<Message[]> {
     }
 }
 
-async function saveNewMessage({type = 'site', description = '', message = '', start = null, end = null, active = 0}:Message):Promise<Message|null> {
+async function saveNewMessage(message: Omit<Message, 'id'>): Promise<Message | null> {
     try {
         const query = `INSERT INTO b2b_oscommerce.site_messages (type, description, message, start, end, active)
                        VALUES (:type, :description, :message, :start, :end, :active)`;
-        const data = {type, description, message, start, end, active};
+        const data = {
+            type: message.type ?? 'site',
+            description: message.description,
+            message: message.message,
+            start: message.start,
+            end: message.end,
+            active: message.active ?? false
+        };
         const [result] = await mysql2Pool.query<ResultSetHeader>(query, data);
         const [_message] = await loadMessages(result.insertId);
         return _message ?? null;
-    } catch(err:unknown) {
+    } catch (err: unknown) {
         if (err instanceof Error) {
             debug("saveNewMessage()", err.message);
             return Promise.reject(err);
@@ -81,18 +88,10 @@ async function saveNewMessage({type = 'site', description = '', message = '', st
 
 }
 
-export async function saveMessage({
-                                      id = 0,
-                                      type = 'site',
-                                      description = '',
-                                      message = '',
-                                      start = null,
-                                      end = null,
-                                      active = 0
-                                  }:Message):Promise<Message|null> {
+export async function saveMessage(message: Omit<Message, 'id'> & Partial<Pick<Message, 'id'>>): Promise<Message | null> {
     try {
-        if (!id) {
-            return await saveNewMessage({type, description, message, start, end, active});
+        if (!message.id) {
+            return await saveNewMessage(message);
         }
         const query = `UPDATE b2b_oscommerce.site_messages
                        SET type = :type,
@@ -102,11 +101,19 @@ export async function saveMessage({
                            end = :end,
                            active = :active
                        WHERE id = :id`;
-        const data = {id, type, description, message, start, end, active};
+        const data = {
+            id: message.id,
+            type: message.type,
+            description: message.description,
+            message: message.message,
+            start: message.start,
+            end: message.end,
+            active: message.active ?? false
+        };
         await mysql2Pool.query(query, data);
-        const [_message] = await loadMessages(id);
+        const [_message] = await loadMessages(message.id);
         return _message ?? null;
-    } catch(err:unknown) {
+    } catch (err: unknown) {
         if (err instanceof Error) {
             debug("saveMessage()", err.message);
             return Promise.reject(err);
@@ -116,13 +123,15 @@ export async function saveMessage({
     }
 }
 
-export async function deleteMessage(id:number|string) {
+export async function deleteMessage(id: number | string) {
     try {
-        const query = `DELETE FROM b2b_oscommerce.site_messages WHERE id = :id`;
+        const query = `DELETE
+                       FROM b2b_oscommerce.site_messages
+                       WHERE id = :id`;
         const data = {id};
         await mysql2Pool.query(query, data);
         return await loadMessages();
-    } catch(err:unknown) {
+    } catch (err: unknown) {
         if (err instanceof Error) {
             debug("deleteMessage()", err.message);
             return Promise.reject(err);
